d7df4f42ae7b013c600c8c37f5e3d918b4c6231d
Sergey Nuzhdin <ipaq.lw@gmail.com>
2019-10-05 13:46:03 +0100

    build helm3

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+HELM_RELEASE=$1
+
 set -euo pipefail
 
 export DOCKER_REPO=lwolf/helm-kubectl-docker
 export K8S_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/kubernetes/kubernetes/releases/latest | cut -d '/' -f 8)
-export HELM_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/helm/helm/releases/latest | cut -d '/' -f 8)
-
+if [ "$HELM_RELEASE" == "helm3" ];then
+  export HELM_VERSION=$(curl -Ls https://github.com/helm/helm/releases | grep "/helm/helm/releases/tag/v3" | head -n 1 | sed 's/[^"]*"\([^"]*\)"[^"]*/\1/g' | cut -d '/' -f 6)
+else
+  export HELM_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/helm/helm/releases/latest | cut -d '/' -f 8)
+fi
 export RELEASE=${K8S_VERSION}-${HELM_VERSION}
 
 docker manifest inspect ${DOCKER_REPO}:${RELEASE} > /dev/null && echo "Version ${RELEASE} is already exists" && exit 0
